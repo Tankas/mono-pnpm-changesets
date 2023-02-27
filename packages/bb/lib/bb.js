618f503a20faedd51889379964f7804894f25d55
tanka <>
2023-02-27 17:54:35 +0800

    依赖包测试

--- packages/bb/lib/bb.js
+++ packages/bb/lib/bb.js
@@ -2,7 +2,7 @@
  * @Author: tanka 
  * @Date: 2023-02-27 16:55:30
  * @LastEditors: tanka 
- * @LastEditTime: 2023-02-27 17:35:47
+ * @LastEditTime: 2023-02-27 17:54:15
  * @FilePath: /mono-pnpm-changesets/packages/bb/lib/bb.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,5 +11,5 @@
 module.exports = bb;
 
 function bb() {
-    return "Hello from bb v5";
+    return "Hello from bb v=7";
 }
