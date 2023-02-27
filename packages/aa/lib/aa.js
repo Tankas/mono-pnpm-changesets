b1ef986219188158a7a6c63d9f80d671b0c93f86
tanka <>
2023-02-27 17:36:20 +0800

    upgrade v5

--- packages/aa/lib/aa.js
+++ packages/aa/lib/aa.js
@@ -1,4 +1,19 @@
+/*
+ * @Author: tanka 
+ * @Date: 2023-02-27 16:54:36
+ * @LastEditors: tanka 
+ * @LastEditTime: 2023-02-27 17:35:05
+ * @FilePath: /mono-pnpm-changesets/packages/aa/lib/aa.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 'use strict';
+
+const bb = require('@hs/bb');
+
+
+const ans = bb();
+
+console.log('ans', bb)
 
 module.exports = aa;
 
