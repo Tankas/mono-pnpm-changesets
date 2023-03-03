bc9c80211e859b009fc2183ed3f442daf9588665
tanka <>
2023-03-03 10:51:12 +0800

    j

--- packages/aa/lib/aa.js
+++ packages/aa/lib/aa.js
@@ -2,7 +2,7 @@
  * @Author: tanka 
  * @Date: 2023-02-27 16:54:36
  * @LastEditors: tanka 
- * @LastEditTime: 2023-03-02 20:32:23
+ * @LastEditTime: 2023-03-02 20:56:08
  * @FilePath: /mono-pnpm-changesets/packages/aa/lib/aa.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -19,11 +19,8 @@ const aa = () => {
     return 'ss'
 }
 
-module.exports = aa;
-
-
-
-
 const es6Test = new Promise(() => {
     
 })
+
+export default aa
