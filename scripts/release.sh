16844255a1d48568bb9cab2df2ac9fed658d5c9c
tanka <>
2023-03-03 15:37:33 +0800

    m

---
+++ scripts/release.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+###
+ # @Author: tanka 
+ # @Date: 2023-03-03 15:36:01
+ # @LastEditors: tanka 
+ # @LastEditTime: 2023-03-03 15:36:50
+ # @FilePath: /mono-pnpm-changesets/scripts/release.sh
+ # @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+### 
+git add .
+git commit -m "m"
+lerna publish
